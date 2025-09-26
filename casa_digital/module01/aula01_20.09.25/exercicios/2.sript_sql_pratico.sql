8aa8b399ecaa4df98f4f53c1e8a3b4743c436a60
guilhermeclementedev <guilhermeclementedealmeida@gmail.com>
2025-09-25 21:56:53 -0300

    feat: :sparkles: add second exercise

--- casa_digital/module01/aula01_20.09.25/exercicios/2.sript_sql_pratico.sql
+++ casa_digital/module01/aula01_20.09.25/exercicios/2.sript_sql_pratico.sql
@@ -32,18 +32,71 @@ CREATE TABLE allocate_employees_project(
 	allocate_end_data DATE
 );
 
--- Populando projetos
-INSERT INTO projects (project_name, project_status)
-VALUES ('Cub3d', 'in_progress');
 INSERT INTO projects (project_name, project_status, start_data, end_data)
-VALUES ('Minishell', 'completed', '2025-04-01', '2025-06-20');
-INSERT INTO projects (project_name, project_status, start_data, end_data)
-VALUES ('So_long', 'completed', '2025-03-01', '2025-05-20');
-INSERT INTO projects (project_name, project_status, start_data, end_data)
-VALUES ('Philosophers','completed', '2025-06-01', '2025-07-20');
-INSERT INTO projects (project_name)
-VALUES ('NetPractice');
+VALUES
+('Cub3d', 'in_progress', '2025-09-01', NULL),
+('Minishell', 'completed', '2025-04-01', '2025-06-20'),
+('So_long', 'completed', '2025-03-01', '2025-05-20'),
+('Philosophers', 'completed', '2025-06-01', '2025-07-20'),
+('NetPractice', 'planning', '2025-09-25', NULL);
 
+INSERT INTO employees (employee_name, employee_role, start_data, end_data, employee_salary)
+VALUES
+('Luiz', 'Tech Lead', '2025-01-01', NULL, 12000),
+('Guilherme', 'Dev Junior', '2025-02-15', NULL, 3000),
+('Gabriel', 'Dev Pleno', '2023-03-01', '2025-09-01', 5000),
+('Beatriz', 'Dev Senior', '2021-01-20', '2025-08-31', 10000),
+('Juninho', 'Dev Junior', '2025-04-10', NULL, 3200);
 
+INSERT INTO allocate_employees_project (employee_id, project_id, allocate_data, allocate_end_data)
+VALUES
+(1, 1, '2025-09-01', NULL),
+(2, 1, '2025-09-01', NULL),
+(2, 2, '2025-04-01', '2025-06-20'),
+(3, 2, '2025-04-01', '2025-06-20'),
+(3, 3, '2025-03-01', '2025-05-20'),
+(4, 4, '2025-06-01', '2025-07-20'),
+(5, 5, '2025-09-25', NULL);
+
+-- Verificando todas as tabelas após criação
+/*
 SELECT *
 FROM projects;
+SELECT *
+FROM employees;
+SELECT *
+FROM allocate_employees_project;
+*/
+
+-- 3 Consultas usando where
+-- Buscando todos os projetos Distintos que ainda não foram finalizados
+SELECT DISTINCT *
+FROM projects
+WHERE end_data IS NULL
+OR project_status <> 'completed';
+-- '<>' -> significa diferente
+
+-- Busca todos os nomes e salarios dos funcionarios que são Junior ainda ativos
+SELECT employee_name, employee_salary
+FROM employees
+WHERE employee_role = 'Dev Junior'
+AND end_data IS NULL;
+
+-- Buscar funcionarios que recebem mais de 5000 e estão ativos
+SELECT *
+FROM employees
+WHERE employee_salary >= 5000
+AND end_data IS NULL;
+
+-- 1 Consulta usando ORDER BY
+-- Buscando quem é mais antigo de casa
+SELECT *
+FROM employees
+ORDER BY start_data;
+
+-- 1 consulta com JOIN
+-- Listar projetos concluídos com os funcionários que participaram.
+SELECT p.project_name, p.project_status, e.employee_name, e.employee_role
+FROM allocate_employees_project a
+JOIN employees e ON a.employee_id = e.employee_id
+JOIN projects p ON a.project_id = p.project_id;
--- casa_digital/module01/aula01_20.09.25/exercicios/respostas.md
+++ casa_digital/module01/aula01_20.09.25/exercicios/respostas.md
@@ -2,3 +2,12 @@
 Pesquisei superficialmente os bancos de dados apresentados, mas escolhi me aprofundar um pouco mais no Oracle Database. Ele é um SGBD muito robusto e seguro, amplamente utilizado em grandes empresas (bancos, seguradoras, empresas de telecomunicação etc) onde falhas não são aceitáveis. Seus principais pontos positivos são: altíssimo desempenho, confiabilidade e escalabilidade. Já seus pontos negativos incluem o custo extremamente elevado e a alta complexidade de administração.
 
 ## 2. Script SQL Prático
+[Ver script SQL](2.script_sql_pratico.sql)
+
+## 3. Modelagem de Sistema
+
+## 4. Leitura Complementar
+
+## 5. Reflexão Crítica
+
+## 6. Prática com Dataset Real
